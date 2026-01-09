045fe7394718040c28e9f19a85271d63251e0523
arpit1344 <akstudy921@gmail.com>
2026-01-10 03:03:14 +0530

    Update workers.py

--- workers.py
+++ workers.py
@@ -2,11 +2,8 @@ import asyncio
 from redis_queue import pop
 from telethon.tl.types import MessageMediaWebPage
 
-# uses globals from main.py:
-# client, CONFIG, STATS, SYSTEM_PAUSED, AUTO_SCALE
-# auto_scale()
-
 async def worker(bot_key):
+
     while True:
 
         if SYSTEM_PAUSED:
@@ -23,20 +20,27 @@ async def worker(bot_key):
 
         batch = bot.get("batch", 10)
         interval = bot.get("interval", 1800)
-
         sent = 0
 
         for src in bot.get("sources", []):
+
             while sent < batch:
                 msg = pop(bot_key, src)
                 if not msg:
                     break
 
                 try:
-                    await client.send_message(
-                        bot["username"],
-                        msg.get("text") or ""
-                    )
+                    if msg.get("has_media"):
+                        await client.send_file(
+                            bot["username"],
+                            msg["media"],
+                            caption=msg.get("text")
+                        )
+                    else:
+                        await client.send_message(
+                            bot["username"],
+                            msg.get("text") or ""
+                        )
 
                     STATS[bot_key]["total"] += 1
                     STATS[bot_key]["sources"].setdefault(str(src), 0)
